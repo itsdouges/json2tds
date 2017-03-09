b8019aa529071bdf06b6a61e2887c705c8bda3c7
Matthew Dismorr <Matthew.Dismorr@mi9.com.au>
2017-03-09 19:19:59 +1100

    Almost did a thing, reading file and forming json object

--- src/lib/itemParser.js
+++ src/lib/itemParser.js
@@ -1,9 +1,24 @@
+import fs from 'fs';
+
 export default function parseItem(path) {
 
-  const cleanFile = clean(path);
-  const fieldsString = cleanFile.split('/\n----\S+----,/');
-  fieldString.forEach((field) => {
-    const listofValues = field.split(',');
+  const data = fs.readFileSync(path, 'utf8')
+  const fieldsString = data.toString().replace('\n', ',\n').split('/----\S+----,/');
+
+  console.log(fieldsString);
+  console.log('LENGTH', fieldsString.length);
+
+  const jsonFields = [];
+  fieldsString.forEach((field) => {
+    const listOfValues = field.split(',');
+    const fields = {};
+    listOfValues.forEach((row) => {
+      const keyValuePair = row.split(': ');
+      const key = keyValuePair[0];
+      const value = keyValuePair[1];
+      fields[key] = value;
+    });
+	  jsonFields.push(fields);
   });
 
   return {
@@ -12,8 +27,4 @@ export default function parseItem(path) {
     path: '/sitecore/content/Home',
     icon: 'Applications/16x16/form_blue.png',
   };
-}
-
-clean = (file) => {
-  return (file.replace('\n\n', '\n')).replace('\n', ',\n');
 }
