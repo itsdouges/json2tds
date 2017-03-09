2887cd8dce06f35d22a81738cea96429a078a9f1
Matthew Dismorr <Matthew.Dismorr@mi9.com.au>
2017-03-09 19:42:29 +1100

    More of a thing, actually uses regex now

--- src/lib/itemParser.js
+++ src/lib/itemParser.js
@@ -3,22 +3,29 @@ import fs from 'fs';
 export default function parseItem(path) {
 
   const data = fs.readFileSync(path, 'utf8')
-  const fieldsString = data.toString().replace('\n', ',\n').split('/----\S+----,/');
-
-  console.log(fieldsString);
-  console.log('LENGTH', fieldsString.length);
-
+  const fieldsString = data.toString().replace(/\n/g, ',').split(/----\S+----,/);
   const jsonFields = [];
+
   fieldsString.forEach((field) => {
     const listOfValues = field.split(',');
+
     const fields = {};
     listOfValues.forEach((row) => {
-      const keyValuePair = row.split(': ');
-      const key = keyValuePair[0];
-      const value = keyValuePair[1];
-      fields[key] = value;
+      if (row) {
+        const keyValuePair = row.split(': ');
+        const key = keyValuePair[0];
+        const value = keyValuePair[1];
+
+        if (key && value) {
+          fields[key] = value;
+        }
+        else {
+          fields['value'] = key;
+        }
+      }
     });
 	  jsonFields.push(fields);
+    console.log(jsonFields);
   });
 
   return {
